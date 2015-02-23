178e759a4716c418812fa3d38c7aaae5228c4356
spencernewell93 <spencernewell93@gmail.com>
2015-02-23 12:57:05 -0600

    commit for merge

--- app/src/main/java/com/example/spencer/swirlytap/singlePlayer.java
+++ app/src/main/java/com/example/spencer/swirlytap/singlePlayer.java
@@ -185,7 +185,7 @@ public class singlePlayer extends ActionBarActivity
     private void gridButtonClicked(int row, int col) //any time a button clicked do something
     {
         //determine what button is. if good then add point, if bad take away point, if 2x then 2 times points
-        buttons[row][col].setBackgroundColor(Color.TRANSPARENT);
+
         count++;
 
     }
