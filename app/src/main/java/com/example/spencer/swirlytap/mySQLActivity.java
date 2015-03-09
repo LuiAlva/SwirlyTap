fb4edb532fa6d0ccba945abf495afc362c0274db
EbereAko <ebereako@gmail.com>
2015-03-08 22:34:49 -0700

    Starting over on DataBase and usining SQLITE instead

--- app/src/main/java/com/example/spencer/swirlytap/mySQLActivity.java
+++ app/src/main/java/com/example/spencer/swirlytap/mySQLActivity.java
@@ -1,108 +1,71 @@
 
 package com.example.spencer.swirlytap;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Toast;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Button;
 
 
-public class mySQLActivity extends Activity
-{
-    EditText editName,Password;
-    Button bSubmit;
-    @SuppressLint("NewApi")
+public class mySQLActivity extends ActionBarActivity implements View.OnClickListener{
+
+    Button buttonLogIn;
+    Button buttonRegister;
+    Button buttonUpdate;
+    Button buttonDelete;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
         setContentView(R.layout.activity_my_sql);
-        editName = (EditText)findViewById(R.id.editName);
-        Password = (EditText)findViewById(R.id.Password);
-        bSubmit = (Button)findViewById(R.id. Submit);
-        bSubmit.setOnClickListener(new View.OnClickListener(){
-            InputStream is = null;
+        buttonLogIn = (Button)findViewById(R.id.LogIn);
+        buttonLogIn.setOnClickListener(this);     //sets an onClickListener on buttonLogIn
+        buttonRegister = (Button)findViewById(R.id.Register);
+        buttonRegister.setOnClickListener(this);      //sets an onClickListener on buttonRegister
+        buttonUpdate = (Button)findViewById(R.id.UpDate);
+        buttonUpdate.setOnClickListener(this);     //sets an onClickListener on buttonUpdate
+        buttonDelete = (Button)findViewById(R.id.Delete);
+        buttonDelete.setOnClickListener(this);
 
-            @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-            @Override
-            public void onClick(View arg0)
-            {
-                String name = editName.getText().toString();
-                String password = Password.getText().toString();
-                //String score = --+Score.getText().toString();
+    }
+    private void LogInClick()
+    {
 
-                List<NameValuePair> nameValuePairs;
-                nameValuePairs = new ArrayList<>(1);
-                nameValuePairs.add(new BasicNameValuePair("Name", name));
-                nameValuePairs.add(new BasicNameValuePair("Password",password));
+    }
+    private void RegisterClick()
+    {
 
-                //}
-                try
+    }
+    private void UpDateClick()
+    {
 
-                {
-                    HttpClient httpClient = new DefaultHttpClient();
-
-                    HttpPost httpPost = new HttpPost("http://10.0.2.2/tutorial.php");
-
-
-                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-                    HttpResponse response = httpClient.execute(httpPost);
-
-                    HttpEntity entity = response.getEntity();
-                    is = entity.getContent();
-                    String msg = "Data entered successfully";
-                    Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
-
-                }
-                catch(ClientProtocolException e)
-
-                {
-                    Log.e("ClientProtocol", "Log_tag");
-                    e.printStackTrace();
-                }
-                catch(IOException e)
-
-                {
-                    Log.e("Log_tag", "IOException");
-                    e.printStackTrace();
-                }
-
-
-
-
-
-
-            }
-
-        });
-
+    }
+    private void DeleteClick()
+    {
 
     }
 
-}
 
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.LogIn:
+                LogInClick();
+                break;
+            case R.id.Register:
+                RegisterClick();
+                break;
+            case R.id.UpDate:
+                UpDateClick();
+                break;
+            case R.id.Delete:
+                DeleteClick();
+                break;
+
+        }
+    }
+    }
 
