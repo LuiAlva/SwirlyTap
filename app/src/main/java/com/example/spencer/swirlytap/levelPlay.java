310ca6edd678d6e6954b45266a2dee836232d602
spencernewell93 <spencernewell93@gmail.com>
2015-03-09 18:26:36 -0500

    added level to level mode

--- app/src/main/java/com/example/spencer/swirlytap/levelPlay.java
+++ app/src/main/java/com/example/spencer/swirlytap/levelPlay.java
@@ -3,6 +3,8 @@ package com.example.spencer.swirlytap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -16,6 +18,11 @@ public class levelPlay extends Activity implements View.OnClickListener
 {
     final int NUM_ROWS = 7;
     final int NUM_COLS = 10;
+    String[][] luckArray = new String[NUM_ROWS][NUM_COLS];
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];   //created total number of grid buttons
+    int game_speed = 400;
+    int Current_Time = 61000;
+    int score = 0;
     int level = 1;
     LinearLayout llayout; //set it up after declaration
 
@@ -34,7 +41,24 @@ public class levelPlay extends Activity implements View.OnClickListener
 
         //POPULATE luck array with different types of buttons
         Random rand = new Random(); //randomly select location in luck array
-
+        for(int row = 0; row<NUM_ROWS; row++)
+        {
+            for(int col = 0; col<NUM_COLS; col++)
+            {
+                //bad button should display in a more random fashion...please test
+                int randCell = rand.nextInt(NUM_ROWS*NUM_COLS); //random selection of numbers
+                if(randCell%5 ==0) //much less chance to receive bad button
+                {
+                    luckArray[row][col] = "bad"; //place bad button in array
+                }
+                else if(randCell%6 == 0)
+                {
+                    luckArray[row][col] = "twicePoints"; //place 2x button in array
+                }
+                else
+                    luckArray[row][col] = "good"; //much higher chance to receive good button
+            }
+        }
     }
 
     private void populateButtons() //creating grid of buttons. These buttons are initialized as disabled and invisible
@@ -66,6 +90,7 @@ public class levelPlay extends Activity implements View.OnClickListener
                     }
                 });
                 tableRow.addView(Swirl);
+                buttons[row][col] = Swirl;
 
             }
         }//end 'for'
@@ -82,10 +107,155 @@ public class levelPlay extends Activity implements View.OnClickListener
         switch(lvl)
         {
             case 1:
-            llayout.setBackgroundResource(R.drawable.levelone);
-            break;
+                llayout.setBackgroundResource(R.drawable.levelone);
+                gameTimer(1);
+                break;
+            case 2:
+                llayout.setBackgroundResource(R.drawable.leveltwo);
+                gameTimer(2);
+                break;
         }
     }
+
+    public void gameTimer(final int currentLevel)
+    {
+        new CountDownTimer(60000, 1000)
+        {
+            int newScore = score;
+            public void onTick(long millisUntilFinished)
+            {
+                displayButton(currentLevel);
+                if (score > newScore + 10)
+                {
+                    onFinish();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                goToLevel(currentLevel+1);
+            }
+
+
+
+        }.start();
+
+    }
+    public void displayButton(int levelNum) //will call when button needs to be displayed
+    {
+        final Runnable buttonRunnable;
+        final Handler buttonHandler = new Handler();
+        Random r = new Random(); //randomly select location in luck array
+        int randRow = r.nextInt(NUM_ROWS);
+        int randCol = r.nextInt(NUM_COLS);
+
+        if(luckArray[randRow][randCol]=="good") //GOOD BUTTON +1 POINT IF CLICKED
+        {
+            final Button goodButton = buttons[randRow][randCol];     //Button in this location
+            goodButton.setBackgroundResource(R.drawable.goodswirl); //Set image to goodswirl
+            goodButton.setEnabled(true);                            //Enable Swirl
+            goodButton.setVisibility(View.VISIBLE);                 //Make Swirl Visible
+            buttonRunnable = new Runnable() { //what will be called if button has not been clicked
+                public void run()
+                {
+
+                    goodButton.setVisibility(View.INVISIBLE);
+                    goodButton.setEnabled(false);
+
+                }
+            };
+            if(goodButton.isEnabled() == true) {
+                buttonHandler.postDelayed(buttonRunnable, 1600); //will disappear after 2 seconds
+            }
+            goodButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+
+                    {
+                        //tapGood.start();                         // Play short confirmation sound
+                       // playGood(tapGood);
+                        //v.startAnimation(disabledAnimation());
+                        v.setVisibility(View.INVISIBLE);         // Make Swirl disappear when clicked
+                        v.setEnabled(false);                     // Disable button
+                        score++;                                 // Add one to score
+                    }
+                }
+            });
+
+
+            //set 1 second timer... if timer reached then make button disappear
+        }
+        else if(luckArray[randRow][randCol] == "bad" && levelNum > 1)
+        {
+            final Button badButton = buttons[randRow][randCol];     //Button in this location
+            badButton.setBackgroundResource(R.drawable.badswirl); //Set image to goodswirl
+            badButton.setEnabled(true);                            //Enable Swirl
+            badButton.setVisibility(View.VISIBLE);                 //Make Swirl Visible
+            buttonRunnable = new Runnable() { //what will be called if button has not been clicked
+                public void run()
+                {
+
+                    badButton.setVisibility(View.INVISIBLE);
+                    badButton.setEnabled(false);
+
+                }
+            };
+            if(badButton.isEnabled() == true) {
+                buttonHandler.postDelayed(buttonRunnable, 1700); //will disappear after 2 seconds
+            }
+            badButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+
+                    {
+                        //tapBad.start();                         // Play short confirmation sound
+                      //  playBad(tapBad);
+                        //v.startAnimation(disabledAnimation());
+                        v.setVisibility(View.INVISIBLE);         // Make Swirl disappear when clicked
+                        v.setEnabled(false);                     // Disable button
+                        score -= 5;                                 // Add one to score
+                    }
+                }
+            });
+
+        }
+        else if(luckArray[randRow][randCol] == "twicePoints" && levelNum == 3)
+        {
+            final Button twiceButton = buttons[randRow][randCol];     //Button in this location
+            twiceButton.setBackgroundResource(R.drawable.twiceswirl); //Set image to goodswirl
+            twiceButton.setEnabled(true);                            //Enable Swirl
+            twiceButton.setVisibility(View.VISIBLE);                 //Make Swirl Visible
+            buttonRunnable = new Runnable() { //what will be called if button has not been clicked
+                public void run()
+                {
+
+                    twiceButton.setVisibility(View.INVISIBLE);
+                    twiceButton.setEnabled(false);
+
+                }
+            };
+            if(twiceButton.isEnabled() == true) {
+                buttonHandler.postDelayed(buttonRunnable, 1600); //will disappear after 2 seconds
+            }
+            twiceButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+
+                    {
+                        //tapGood.start();                         // Play short confirmation sound
+                      //  playGood2(tapGood);
+                        //v.startAnimation(disabledAnimation());
+                        v.setVisibility(View.INVISIBLE);         // Make Swirl disappear when clicked
+                        v.setEnabled(false);                     // Disable button
+                        score+=2;                                 // Add one to score
+                    }
+                }
+            });
+
+        }
+
+    } //End of displaybutton
+
     //switch the level cases...for certain levels change background and run animation
 
 }
